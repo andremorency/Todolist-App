17295ba14c5e59ebd22a87e8134a13cd19303c3b
andremorency <andremorency.genesis@gmail.com>
2022-11-05 02:26:41 +0000

    Initial Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, {useState}  from 'react'
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState([""]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const addTodo = (todo) => {
+    const newTodo = {
+        id: Math.random(),
+        todo: todo,
+    };
+// add the todo to the list
+setList([...list, newTodo]);
 
-export default Home;
+// clear input box
+setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    //Filter out todo with the id
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  }
+  
+  return (
+    <div>
+        <h1>Todo List</h1>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}  />
+        <button onClick={()=> addTodo(input)} >Addd</button>
+        <ul>
+            {list.map((todo) => (
+                <li key={todo.id}>
+                    {todo.todo}
+                    <button onClick={() => deleteTodo(todo.id)} >&times;</button>
+                </li>
+           ) ) }
+
+
+
+        </ul>
+    </div>
+  )
+}
+
+export default Home
